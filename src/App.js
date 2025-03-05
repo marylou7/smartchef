7457f3c4b738c3f41bedd395a239a2b4132d6f84
Marylou <2501402@dundee.ac.uk>
2025-03-05 15:11:06 +0000

    removed homepage template code

--- src/App.js
+++ src/App.js
@@ -25,26 +25,6 @@ function App() {
          <Route path="/" element={<HomePage />} />
        </Routes>
        </div>
-      
-       
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          This is a test!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-   
-    
-
     </div>
     
   );
