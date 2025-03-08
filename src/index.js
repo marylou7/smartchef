31cc9e39fb038beb4db70189ed18156ff3cae669
Marylou <2501402@dundee.ac.uk>
2025-03-08 13:30:28 +0000

    added a back button to recipe page

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter} from 'react-router-dom'; 
+import {BrowserRouter} from 'react-router-dom'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
 
 // If you want your app to work offline and load faster, you can change
