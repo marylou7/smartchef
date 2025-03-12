0107ab99b8b674b2aa05f00c5bd910ea054de794
Marylou <2501402@dundee.ac.uk>
2025-03-12 14:48:11 +0000

    added a basic template

--- src/components/ShoppingLists.js
+++ src/components/ShoppingLists.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import './ShoppingLists.css'; 
+import { Link } from 'react-router-dom'; 
 
 const ShoppingLists = () => {
+  // dummy shopping list data for nwo
+  const shoppingLists = [
+    { id: 1, name: 'Weekly Groceries' },
+    { id: 2, name: 'Party Supplies' },
+    { id: 3, name: 'Essentials' }
+  ];
+
   return (
-    <div>
-      <h1>Shopping Lists</h1>
-      <p>Here you can create shopping lists.</p>
+    <div className="shopping-lists-container">
+      <div className="shopping-list-cards">
+        {shoppingLists.map((list) => (
+          <div key={list.id} className="shopping-list-card">
+            <Link to={`/individualList/${list.id}`} className="shopping-list-name">
+              <h3>{list.name}</h3>
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
