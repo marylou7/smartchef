c5a4a9f14f4318bd37d8d1f4c95666a16a41b2ef
Marylou <2501402@dundee.ac.uk>
2025-03-12 14:25:14 +0000

    fixed reload bug

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'; 
+import { BrowserRouter } from 'react-router-dom';
+
+const Root = () => {
+  useEffect(() => {
+    // checks dark mode state from localStorage when the app is loaded
+    const savedDarkMode = localStorage.getItem('darkMode');
+    if (savedDarkMode !== null) {
+      const mode = JSON.parse(savedDarkMode);
+      // apply the class to the body based on saved state
+      document.body.className = mode ? 'dark' : 'light';
+    }
+  }, []); 
+
+  return (
+    <BrowserRouter basename="/smartchef">
+      <App />
+    </BrowserRouter>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter basename="/smartchef" >
-    <App />
-  </BrowserRouter>
-);
+root.render(<Root />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
