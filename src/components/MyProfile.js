0f77745e2ade7590adb01bdcc3f56bb7f077eb93
Marylou <2501402@dundee.ac.uk>
2025-03-22 17:32:22 +0000

    updated profile ui

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -93,12 +93,22 @@ const MyProfile = () => {
     <div className="container">
 
       <div className="ingredients-container">
-        <h3>View My Ingredients</h3>
         <div className="button-container">
           <button className="ingredients-btn" onClick={handleButtonClick}>
             <FaHamburger className="burger-icon" /> View My Ingredients
           </button>
         </div>
+        <hr className="section-divider" />
+
+      
+
+        <h3>Settings</h3>
+
+        <div className="toggle-container">
+          <label className="toggle-label">Dark Mode</label>
+          <DarkMode />
+        </div>
+
         <hr className="section-divider" />
 
         {/* Dietary Preferences */}
@@ -138,14 +148,7 @@ const MyProfile = () => {
           </div>
         </div>
 
-        <hr className="section-divider" />
-
-        <h3>Settings</h3>
-
-        <div className="toggle-container">
-          <label className="toggle-label">Dark Mode</label>
-          <DarkMode />
-        </div>
+        
         {/* Delete My Data Section */}
         <hr className="section-divider" />
         <div className="delete-container">
